f6bfb5f11dd3414f82b7e71aa12ad105608fa162
jadhavaniket6 <53930976+jadhavaniket6@users.noreply.github.com>
2023-08-02 23:22:00 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,13 @@ app = Flask(__name__)
 # Define a route and its handler
 @app.route('/')
 def hello_world():
-    return 'Hello, World! This is a basic Flask app.'
+    result = 'Hello, World! This is a basic Flask app.'
+
+    # Save the result to a file
+    with open('result.txt', 'w') as f:
+        f.write(result)
+
+    return result
 
 # Run the app if this script is executed directly
 if __name__ == '__main__':
